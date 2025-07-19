c202099c7ca8865a420c4e1caaa03da8017beb50
Segunfai <segunfai111@yandex.ru>
2025-07-19 15:04:32 +0500

    Add homework 1.1

---
+++ first/Homework1_2.java
@@ -0,0 +1,32 @@
+package first;
+
+import java.util.Random;
+
+public class Homework1_2 {
+    public static void main(String[] args) {
+        String[] knb = new String[] {"Камень", "Ножницы", "Бумага"};
+        Random igra = new Random();
+        int vyborVasya = igra.nextInt(knb.length);
+        int vyborPetya = igra.nextInt(knb.length);
+        String vasya = knb[vyborVasya];
+        String petya = knb[vyborPetya];
+        String nichya = "Ничья! Победила дружба!";
+        String pobedaVasi = "Победил Вася!";
+        String pobedaPeti = "Победил Петя!";
+        System.out.println("Выбор игрока Вася: " + vasya);
+        System.out.println("Выбор игрока Петя: " + petya);
+        if (vasya.equals("Ножницы") && petya.equals("Бумага")) {
+            System.out.println(pobedaVasi);
+        } else if (vasya.equals("Бумага") && petya.equals("Ножницы")) {
+            System.out.println(pobedaPeti);
+        } else if (vasya.equals("Камень") && petya.equals("Ножницы")) {
+            System.out.println(pobedaVasi);
+        } else if (vasya.equals("Ножницы") && petya.equals("Камень")) {
+            System.out.println(pobedaPeti);
+        } else if (vasya.equals("Бумага") && petya.equals("Камень")) {
+            System.out.println(pobedaVasi);
+        } else if (vasya.equals("Камень") && petya.equals("Бумага")) {
+            System.out.println(pobedaPeti);
+        } else System.out.println(nichya);
+    }
+}
--- first/Main.java
+++ first/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         Scanner vvod = new Scanner(System.in);
         System.out.println("Введите имя пользователя: ");
