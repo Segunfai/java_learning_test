f31688ccaaf54afc6ef573f0f59e244a74f8c2f8
Segunfai <segunfai111@yandex.ru>
2025-07-24 21:18:00 +0500

    Add new methods but empty obj in App class

--- first/App.java
+++ first/App.java
@@ -6,21 +6,35 @@ import java.util.Random;
 public class App {
 
     public static void main(String[] args) {
-        TV info = new TV();
+
+        //Включаем
+        TV newTv = new TV();
+        newTv.power();
+
         //Показываем информацию о телевизоре
+        TV info = new TV();
         info.setBrand();
 
         info.settings();
 
-        TV newTv = new TV();
-        //Включаем
-        newTv.power();
         //Тыкаем случайный канал
         int sKanal = TV.randomChannel();
         System.out.println("Случайный канал: " + sKanal);
 
         TV.randomChannel();
+        //Включаем / выключаем звук
+        TV mute = new TV();
+        mute.zvuk();
 
+        TV sledKanal = new TV();
+        sledKanal.nextKanal();
+
+        TV predKanal = new TV();
+        predKanal.prevKanal();
+
+        TV whatKanal = new TV();
+        whatKanal.tekKanalInfo();
+
+        newTv.power();
     }
-
 }
--- first/TV.java
+++ first/TV.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 
 public class TV {
 
-    private String[] model = {"plockiy", "shirokiy", "krasiviy"};
+    private final String[] model = {"plockiy", "shirokiy", "krasiviy"};
     private String serialNumber;
     public String brand;
     private String operSystem;
     private static final int[] channels = {1, 2, 3, 4, 6, 7, 8, 9};
-    public String mute;
-    public String unmute;
+    private int tekKanal = 0;
+    public boolean mute = false;
     public String switchOn;
     public String switchOff;
     private boolean mode = false;
@@ -40,15 +40,61 @@ public class TV {
         return channels[randomize];
     }
 
-    public void settings() {
-        System.out.println("Бренд: " + brandim);
+    public void setOs() {
 
+        Scanner vvodOs = new Scanner(System.in);
+        System.out.println("Введите название операционной системы телевизора: ");
+        String osim = vvodOs.nextLine();
+        if (osim != null && !osim.isEmpty()) {
+            operSystem = osim;
+        } else {
+            System.out.println("Операционная система телевизора неопознана!");
+        }
+    }
+
+    public void zvuk() {
+        mute = !mute;
+        System.out.println("Звук " + (mute ? "включен" : "выключен"));
+    }
+
+    public void settings() {
+        System.out.println("Бренд: " + brand);
+        System.out.println("Операционная система: " + operSystem);
         Random harakter = new Random();
         int kakoy = harakter.nextInt(model.length);
-        System.out.println("Модель: " + kakoy);
-
-
-
+        String display = model[kakoy];
+        System.out.println("Модель: " + display);
+        serialNumber = "Секретная информация";
+        System.out.println(serialNumber);
     }
+
+    public void nextKanal() {
+        if (!mode) {
+            System.out.println("Сначала включите телевизор!");
+            return;
+        }
+
+        tekKanal = (tekKanal + 1) % channels.length;
+        System.out.println("Текущий канал: " + channels[tekKanal]);
+    }
+
+    public void prevKanal() {
+        if (!mode) {
+            System.out.println("Сначала включите телевизор!");
+            return;
+        }
+
+        tekKanal = (tekKanal - 1 + channels.length) % channels.length;
+        System.out.println("Текущий канал: " + channels[tekKanal]);
+    }
+
+    public void tekKanalInfo() {
+        if (!mode) {
+            System.out.println("Телевизор выключен!");
+            return;
+        }
+        System.out.println("Сейчас на телевизоре включен канал: " + channels[tekKanal]);
+    }
+}
 
 }
